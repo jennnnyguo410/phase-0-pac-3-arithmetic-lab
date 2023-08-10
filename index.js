4117ed7c94e155759b3c80290c73ad398eedbd07
Hao Guo <jennnnyguo@gmail.com>
2023-08-10 22:30:31 +1000

    commit tree 8c1275f00725a6a62aa12c32a6b0e3de7090c620

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function add(a, b) {
-    return a + b;
+    return a + b
 }
 
 function subtract(a, b) {
-    return a - b;
+    return a - b
 }
 
 function multiply(a, b) {
-    return a * b;
+    return a * b
 }
 
 function divide(a, b) {
-    return a / b;
+    return a / b
 }
 
-function increment(n) {
-    return n += 1;
+function increment(a) {
+    return a + 1
 }
 
-function decrement(n) {
-    return n -= 1;
+function decrement(a) {
+    return a - 1
 }
 
-function makeInt(n) {
-    return parseInt(n, 10);
+function makeInt(a) {
+    return parseInt(a, 10)
 }
 
 function preserveDecimal(n) {
-    return parseFloat(n);
+    return parseFloat(n)
 }
